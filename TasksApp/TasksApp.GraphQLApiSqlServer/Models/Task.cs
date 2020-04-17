3142bcbd39640c12c606dfea86c54b026cd27b67
Sixto Ortega <sixto_miguel@hotmail.com>
2020-04-17 11:51:40 -0400

    Adding model task

---
+++ TasksApp/TasksApp.GraphQLApiSqlServer/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasksApp.GraphQLApiSqlServer.Models
+{
+    public class Task
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
