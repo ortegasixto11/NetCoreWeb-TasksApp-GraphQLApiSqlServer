89e632dcd5b97e94a172ff15a7c5c675901e84f4
Sixto Ortega <sixto_miguel@hotmail.com>
2020-04-17 11:52:13 -0400

    Adding database context and migration

---
+++ TasksApp/TasksApp.GraphQLApiSqlServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasksApp.GraphQLApiSqlServer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext() : base(new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseSqlServer(GetConnectionString()).Options)
+        {
+        }
+
+        private static string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+            return configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public virtual DbSet<Models.Task> Tasks { get; set; }
+    }
+}
---
+++ TasksApp/TasksApp.GraphQLApiSqlServer/Migrations/20200417154806_Initial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TasksApp.GraphQLApiSqlServer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
